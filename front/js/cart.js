36813776ed7c6a031bec62159bf5f9bb59da8fb4
Assia-B00 <assia.bouchlaghem@outlook.com>
2023-03-26 14:40:37 +0200

    clean cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -30,9 +30,6 @@ function addToCart(product_id, color, quantity) {
 }
 
 // Affichage du panier avec les données des produits dans le tableau
-// Parcourir le panier (cart)
-// Appel au fetch pour récupréer les données du produit
-// Affiche les données
 
 function get_cart() {
     let cart = JSON.parse(localStorage.getItem('cartProduct'));
@@ -45,131 +42,106 @@ function get_cart() {
     // Si le panier n'est pas vide : afficher les produits du local storage
     // Pour parcourir le panier, on fait une boucle for 
     for (let i = 0; i < cart.length; i++) {
-        fetch("http://localhost:3000/api/products/" + cart[i].id) // Ne pas confondre localstorage et base de données
+        fetch("http://localhost:3000/api/products/" + cart[i].id)
             .then(function (response) {
                 if (response.ok) {
                     return response.json();
                 }
             })
             .then(function (product) {
-                
-                    let totalPrice = 0;
-                    let article = document.getElementById("cart__items")
 
-                    // Pour séléctionner la class où j'injecte le code HTML
-                    //const list = document.querySelector('.cart__item');
-                    //console.log("je suis là" + list);
+                let totalPrice = 0;
+                let article = document.getElementById("cart__items")
 
-                    // listProduct.innerHTML = '${cart.imageURL} ${cart.name} ${cart.color} ${cart.price}€ ${cart.quantity}';
+                // Pour afficher la balise <article> du code HTML : 
+                let seeCart = document.createElement("article")
+                seeCart.classList.add("cart__item")
+                seeCart.setAttribute("data-id", product["_id"])
+                seeCart.setAttribute("data-color", cart[i]["color"])
 
-                    /*
-                    <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                    */
-                    let seeCart = document.createElement("article")
-                    seeCart.classList.add("cart__item")
-                    seeCart.setAttribute("data-id", product["_id"])
-                    seeCart.setAttribute("data-color", cart[i]["color"])
+                // Pour afficher l'image du produit :
+                let div_img = document.createElement("div")
+                div_img.classList.add("cart__item__img")
 
-                    /*                       
-                        <div class="cart__item__img">
-                            <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-                        </div>
-                    */
-                    let div_img = document.createElement("div")
-                    div_img.classList.add("cart__item__img")
+                let img = document.createElement("img")
+                img.setAttribute("src", product["imageUrl"])
+                img.setAttribute("alt", product["altTxt"])
+                div_img.appendChild(img)
 
-                    let img = document.createElement("img")
-                    img.setAttribute("src", product["imageUrl"])
-                    img.setAttribute("alt", product["altTxt"])
-                    div_img.appendChild(img)
-                                       
-                    article.appendChild(div_img)
+                article.appendChild(div_img)
 
-                    /*
-                    <div class="cart__item__content">
-                    */
-                    let cart_content = document.createElement("div")
-                    cart_content.classList.add("cart__item__content")
+                //Pour afficher la <div class="cart__item__content"> :
+                let cart_content = document.createElement("div")
+                cart_content.classList.add("cart__item__content")
 
-                    /*
-                    <div class="cart__item__content__description">
-                        <h2>Nom du produit</h2>
-                        <p>Vert</p>
-                        <p>42,00 €</p>
-                    </div>
-                    */
-                    let cartDescription = document.createElement("div")
-                    cartDescription.classList.add("cart__item__content__description")
+                //Pour afficher la <div class="cart__item__content__description"> qui contient le nom, la couleur et le prix du produit : 
+                let cartDescription = document.createElement("div")
+                cartDescription.classList.add("cart__item__content__description")
 
-                    let h2 = document.createElement("h2")
-                    h2.innerHTML = product.name;
-                    cartDescription.appendChild(h2)
+                // Pour afficher le nom du produit : 
+                let h2 = document.createElement("h2")
+                h2.innerHTML = product.name;
+                cartDescription.appendChild(h2)
 
-                    let color = document.createElement("p")
-                    color.innerHTML = cart[i].color;
-                    cartDescription.appendChild(color)
+                // Pour afficher la couleur du produit : 
+                let color = document.createElement("p")
+                color.innerHTML = cart[i].color;
+                cartDescription.appendChild(color)
 
-                    let price = document.createElement("p")
-                    price.innerHTML = parseFloat(product.price).toFixed(2) + " €"; // Pour avoir décimale (2choiffres après la virgule)
-                    
-                    totalPrice += product.price * cart[i].quantity
+                // Pour afficher le prix total du produit :
+                let price = document.createElement("p")
+                price.innerHTML = parseFloat(product.price).toFixed(2) + " €"; // Pour avoir décimale (2 chiffres après la virgule)
 
-                    cartDescription.appendChild(price)
+                totalPrice += product.price * cart[i].quantity
 
-                    cart_content.appendChild(cartDescription)
+                cartDescription.appendChild(price)
 
-                    article.appendChild(cart_content)
-                    /*
-                    <div class="cart__item__content__settings">
-                        <div class="cart__item__content__settings__quantity">
-                            <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                        </div>
-                        <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
-                        </div>
-                    </div>
-                    */
-                    let cartChoice = document.createElement("div")
-                    cartChoice.classList.add("cart__item__content__settings")
+                cart_content.appendChild(cartDescription)
 
-                    let choiceQuantity = document.createElement("div")
-                    choiceQuantity.classList.add('cart__item__content__settings__quantity')
+                article.appendChild(cart_content)
 
-                    let p = document.createElement("p")
-                    p.innerHTML = "Qté :"
-                    choiceQuantity.appendChild(p)
+                // Pour afficher la <div class="cart__item__content__settings">
+                let cartChoice = document.createElement("div")
+                cartChoice.classList.add("cart__item__content__settings")
 
-                    let inputQuantity = document.createElement("input")
-                    inputQuantity.setAttribute("type", "number")
-                    inputQuantity.setAttribute("name", "itemQuantity")
-                    inputQuantity.setAttribute("min", "1")
-                    inputQuantity.setAttribute("max", "100")
-                    inputQuantity.setAttribute("value", cart[i].quantity)
-                    inputQuantity.classList.add("itemQuantity")
+                // Pour afficher la quantité : 
+                let choiceQuantity = document.createElement("div")
+                choiceQuantity.classList.add('cart__item__content__settings__quantity')
 
-                    choiceQuantity.appendChild(inputQuantity)
-                    cartChoice.appendChild(choiceQuantity)
+                let p = document.createElement("p")
+                p.innerHTML = "Qté :"
+                choiceQuantity.appendChild(p)
 
-                    let btnDelete = document.createElement("div")
-                    btnDelete.classList.add("cart__item__content__settings__delete")
+                let inputQuantity = document.createElement("input")
+                inputQuantity.setAttribute("type", "number")
+                inputQuantity.setAttribute("name", "itemQuantity")
+                inputQuantity.setAttribute("min", "1")
+                inputQuantity.setAttribute("max", "100")
+                inputQuantity.setAttribute("value", cart[i].quantity)
+                inputQuantity.classList.add("itemQuantity")
 
-                    let pDelete = document.createElement("p")
-                    pDelete.classList.add("deleteItem")
-                    pDelete.innerHTML = "Supprimer"
-                    btnDelete.appendChild(pDelete)
-                    
-                    cartChoice.appendChild(btnDelete)
-                    cart_content.appendChild(cartChoice)
+                // Pour afficher le bouton supprimer :
+                choiceQuantity.appendChild(inputQuantity)
+                cartChoice.appendChild(choiceQuantity)
 
-                    // Ajout du prix total
-                    /*
-                    <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
-                    */
-                   document.getElementById("totalQuantity").innerHTML = cart.length
-                   document.getElementById("totalPrice").innerHTML = parseFloat(totalPrice).toFixed(2)
+                let btnDelete = document.createElement("div")
+                btnDelete.classList.add("cart__item__content__settings__delete")
 
-                
+                let pDelete = document.createElement("p")
+                pDelete.classList.add("deleteItem")
+                pDelete.innerHTML = "Supprimer"
+                btnDelete.appendChild(pDelete)
+
+                cartChoice.appendChild(btnDelete)
+                cart_content.appendChild(cartChoice)
+
+                // Pour afficher le nombre total d'articles :
+                document.getElementById("totalQuantity").innerHTML = cart.length
+
+                // Pour afficher le prix TOTAL du panier : 
+                document.getElementById("totalPrice").innerHTML = parseFloat(totalPrice).toFixed(2)
+
+
             })
 
     }
@@ -183,13 +155,18 @@ function modifQuantity() {
 
 // Pour suprimer un article : 
 let deleteProducts = document.querySelectorAll(".deleteItem")
+console.log(deleteProducts)
 for (let j = 0; j < deleteProducts.length; j++) {
-    deleteProducts.addEventListener("click");
-    let theProductDelete = cart[j]._id;
-    cart = cart.filter(products => products._id !== _id);
-    localStorage.setItem("cartProduct", JSON.stringify(cart));
+    deleteProducts.addEventListener("click", (event) => {
+        event.preventDefault();
+        let theProductDelete = cart[j].id;
+        console.log("theProductDelete")
+        console.log(theProductDelete)
+        cart = cart.filter(products => products.id !== _id);
+        localStorage.setItem("cartProduct", JSON.stringify(cart));
+    });
 }
-// Pour afficher le total prix du panier : 
+/*// Pour afficher le total prix du panier : 
 function totalPriceInCart() {
     let total = []
 
